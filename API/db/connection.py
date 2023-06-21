95bf64603f4f03b7f86d11d3dbf55efddd71c9cf
Zeke16 <kr2000.16@gmail.com>
2023-06-21 15:26:27 -0600

    Agregando dotenv

--- API/db/connection.py
+++ API/db/connection.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Db local
 # db_connection = MongoClient().local
 
 # Db en la nube
 
-db_connection = MongoClient(
-    "mongodb+srv://zeke16:NU63BHEGftKHX0oR@cluster01.ufhghso.mongodb.net/?retryWrites=true&w=majority").test
+db_connection = MongoClient(os.environ.get("dbconnectionstring")).test
